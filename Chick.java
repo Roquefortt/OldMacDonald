8fa41136ac8243e41cf2c1cb8c9bbcb1504ff4d8
Roquefortt <xpiezx@gmail.com>
2015-10-26 10:37:22 -0700

    index.html

--- Chick.java
+++ Chick.java
@@ -1,4 +1,31 @@
 class Chick implements Animal 
 {     
-     //your code here
+    private String myType;
+    private String mySound;
+    //private String moreSounds;
+    public Chick(String type, String sound)
+    {
+    	myType = type;
+    	mySound = sound;
+    	//moreSounds = anotherSound;
+    }
+
+    public Chick()
+    {
+    	myType = "unknown";
+    	mySound = "unknown";
+    	//moreSounds = "unknown";
+    }
+
+    public String getSound(){return mySound;}
+    public String getType(){return myType;}
+    //public String getanotherSound(){return moreSounds;}
 }
+
+
+/*It turns out that the chick is a little confused. Sometimes it makes one sound, when she is feeling childish, 
+and another when she is feeling more grown up. Her two sounds are "cheep" and "cluck". 
+Modify the Chick class code to allow a second constructor allowing two possible sounds and the 
+getSound() function to return either sound, with equal probability, if there are two sounds available. 
+You will also have to modify your Farm class code to construct the Chick with two possible sounds.
+*/
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,15 @@
-class NamedCow
+class NamedCow extends Cow
 {
+	private String myName;
+	public NamedCow(String type, String name, String sound)
+	{  
+        myName = name;
+	}
 
+	public void getName()
+	{
+		myName = "unknown";
+	}
+
+	public String myName(){return myName;}
 }
