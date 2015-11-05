dd2df5f5f5ba2290c0e001446a2393cbc250c9b9
Roquefortt <xpiezx@gmail.com>
2015-11-04 19:41:47 -0800

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -4,8 +4,8 @@ class NamedCow extends Cow
 	public NamedCow(String type, String name, String sound)
 	{  
 		myType = type;
-    	mySound = sound;
-        myName = name;
+    		mySound = sound;
+		myName = name;
 	}
 
 	/*public NamedCow()
